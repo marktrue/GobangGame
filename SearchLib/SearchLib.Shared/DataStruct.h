3360d15e03f5563635669adfba21c24f03fa4f57
marktrue <ahu.marktrue@gmail.com>
2016-02-20 23:06:39 +0800

    genNextMove

--- SearchLib/SearchLib.Shared/DataStruct.h
+++ SearchLib/SearchLib.Shared/DataStruct.h
@@ -6,25 +6,61 @@
 #define		INF			0x7fffffff
 
 #define		VALTYPE		int
+#define		VAL_EMPTY	0
+
+#define		CELL		int
+#define		CELL_EMPTY	0
+#define		CELL_BLACK	1
+#define		CELL_WHITE	-1
+
 
 enum NodeType
 {
-	alpha = 1,
-	beta = 2
+	Black = CELL_BLACK,
+	White = CELL_WHITE
+};
+
+NodeType operator !(NodeType &a)
+{
+	return a == NodeType::Black ? NodeType::White : NodeType::Black;
+}
+
+struct Point
+{
+	int x;
+	int y;
+	Point(int a = 0, int b = 0)
+	{
+		x = a;
+		y = b;
+	}
+	bool operator < (Point &obj)
+	{
+		return (x != obj.x ? x < obj.x : y < obj.y);
+	}
+	bool operator == (Point &obj)
+	{
+		return x == obj.x && y == obj.y;
+	}
 };
 
 struct Action
 {
-	int x;
-	int y;
-	NodeType type;
+	Point _point;
+	NodeType _type;
+	Action()
+	{
+		_point.x = 0;
+		_point.y = 0;
+		_type = NodeType::White;
+	}
 };
 
 struct GobangPlate
 {
 	int		width;
 	int		height;
-	int		**plate;
+	CELL	**plate;
 };
 
 struct TreeNode
@@ -35,6 +71,14 @@ struct TreeNode
 	TreeNode	*next;
 	VALTYPE		value;
 	Action		action;
+	TreeNode()
+	{
+		parent = NULL;
+		child = NULL;
+		prev = NULL;
+		next = NULL;
+		value = VAL_EMPTY;
+	}
 };
 
 
--- SearchLib/SearchLib.Shared/SearchLib.cpp
+++ SearchLib/SearchLib.Shared/SearchLib.cpp
@@ -2,6 +2,8 @@
 #include "SearchLib.h"
 #include "DataStruct.h"
 
+#include <set>
+
 #ifndef _WINDLL
 int main()
 {
@@ -9,8 +11,123 @@ int main()
 }
 #endif
 
+GobangPlate gamePlate;
+
+int InitGame(int _width, int _height)
+{
+	gamePlate.width = _width;
+	gamePlate.height = _height;
+	gamePlate.plate = new CELL*[_width];
+	gamePlate.plate[0] = new CELL[_width * _height];
+	for (int i = 0; i < _width * _height; ++i)
+	{
+		gamePlate.plate[0][i] = CELL_EMPTY;
+	}
+	for (int i = 1; i < _width; ++i)
+	{
+		gamePlate.plate[i] = gamePlate.plate[i - 1] + _height;
+	}
+	return 0;
+}
+
+int UnInitGame()
+{
+	delete gamePlate.plate[0];
+	delete gamePlate.plate;
+	return 0;
+}
+
 int genNextMove(TreeNode *node)
 {
+	//只在每个棋子+1,2范围内着子
+	std::set<Point> pnts;
+	int x, y;
+	for (TreeNode *it = node; it != NULL; it = it->parent)
+	{
+		Point one;
+		x = it->action._point.x;
+		y = it->action._point.y;
+		//
+		one.x = x + 1;
+		one.y = y;
+		pnts.insert(one);
+		one.x = x - 1;
+		one.y = y;
+		pnts.insert(one);
+		one.x = x;
+		one.y = y + 1;
+		pnts.insert(one);
+		one.x = x;
+		one.y = y - 1;
+		pnts.insert(one);
+		//
+		one.x = x + 1;
+		one.y = y + 1;
+		pnts.insert(one);
+		one.x = x - 1;
+		one.y = y - 1;
+		pnts.insert(one);
+		one.x = x + 1;
+		one.y = y - 1;
+		pnts.insert(one);
+		one.x = x - 1;
+		one.y = y + 1;
+		pnts.insert(one);
+		//
+		one.x = x + 2;
+		one.y = y;
+		pnts.insert(one);
+		one.x = x - 2;
+		one.y = y;
+		pnts.insert(one);
+		one.x = x;
+		one.y = y + 2;
+		pnts.insert(one);
+		one.x = x;
+		one.y = y - 2;
+		pnts.insert(one);
+		//
+		one.x = x + 2;
+		one.y = y + 2;
+		pnts.insert(one);
+		one.x = x - 2;
+		one.y = y - 2;
+		pnts.insert(one);
+		one.x = x + 2;
+		one.y = y - 2;
+		pnts.insert(one);
+		one.x = x - 2;
+		one.y = y + 2;
+		pnts.insert(one);
+	}
+
+	for (TreeNode *it = node; it != NULL; it = it->parent)
+	{
+		Point one;
+		one = it->action._point;
+		pnts.erase(one);
+	}
+
+	for (std::set<Point>::iterator it = pnts.begin(); it != pnts.end(); it++)
+	{
+		Action newact;
+		newact._point = (Point)*it;
+		newact._type = !node->action._type;
+		if (node->child != NULL)
+		{
+			node->child->next = new TreeNode();
+			node->child->next->action = newact;
+			node->child->next->parent = node;
+			node->child->next->prev = node->child;
+			node->child = node->child->next;
+		}
+		else
+		{
+			node->child = new TreeNode();
+			node->child->action = newact;
+			node->child->parent = node;
+		}
+	}
 	return 0;
 }
 
